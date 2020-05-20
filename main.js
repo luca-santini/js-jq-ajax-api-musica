540b4b61ef752cecb22882adc11e2438a111c28b
Luca Santini <lucasantinidev@gmail.com>
2020-05-20 16:29:18 +0200

    commenti js

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// Con una chiamata ajax, recuperare i dischi musicali restituiti dall'api
+// Ciclare quindi i dischi ottenuti
+// per ognuno di essi disegnare in pagina una card utilizzando handlebars
 $(document).ready(function() {
-	//Code
+
+$.ajax({
+    'url': 'https://flynn.boolean.careers/exercises/api/array/music',
+    'method': 'GET',
+    'success': function(data) {
+        // Code
+    },
+    'error': function() {
+        alert('si è verificato un errore');
+    }
 });
+});
+
+//  creo un oggetto che mi contiene tutti gli elementi dei miei dischi
+var dischi = [
+    {
+        'titolo': 'h3',
+        'autore': '<span class="author"></span>',
+        'anno': '<span class="year"></span>',
+        'foto': 'img src'
+    }
+];
